c7dd198c6a5d23678d07b628d5b650a9195fa8e4
Palisse Volia <palissev@gmail.com>
2022-02-25 13:01:20 +0100

    commit

---
+++ TD1/exercice 2.py
@@ -0,0 +1,9 @@
+#Question 1
+#On perd 15 chifffres significatifs
+
+#Question 2
+xt = 0.001223991124941416
+xr1 = 0.0012239911249
+err = abs(xt-xr1)/abs(xt)
+print("L'erreur de base est :")
+print(err)
