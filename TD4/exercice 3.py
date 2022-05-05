c95b425b80eae68007e2b57524279066a462da44
Volia Palisse <81376099+PalisseVolia@users.noreply.github.com>
2022-05-05 10:19:17 +0200

    jokjo

--- TD4/exercice 3.py
+++ TD4/exercice 3.py
@@ -13,3 +13,12 @@ print()
 #On fait ensuite la solution particulière y'= -150y+30 : Sp = 1/5
 #Par conséquent la solution de l'équa diff est : S = Sh + Sp = Ke^(-150t) + 1/5 = Ke^(-150t) + 1/5 avec y(0)=alpha
 
+def Euler(f, T, y0, n):
+    h = T/n
+    y = [y0]
+    t = [0]
+    for i in range(n):
+        y.append(y[i] + h*f(t[i],y[i]))
+        t.append(t[i] + h)
+    return y
+    
